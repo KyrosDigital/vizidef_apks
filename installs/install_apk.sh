4eda5353fb6574d6dc1adceb4c6ea5ade0befa42
Nathan Jean <nathanjean@kyrosagency.com>
2020-09-30 20:33:45 -0400

    updates

--- installs/install_apk.sh
+++ installs/install_apk.sh
@@ -3,5 +3,6 @@
 # after first connection with ssh  to android device, run these commands
 # this will create a file via ssh on the device that allows for execution 
 # we execute the script which sends the device ssh server and information to the MDM system
+cd ~/vizidef_apks
 
-git pull origin master https://KyrosDigital:Kyrosgenetix2017%27@github.com/KyrosDigital/vizidef_apks.git
+git pull origin master
