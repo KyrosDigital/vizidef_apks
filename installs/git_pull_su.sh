cd17595808c9f50588b921f753c04d374fb93bdb
Nathan Jean <nathanjean@kyrosagency.com>
2020-09-30 21:04:31 -0400

    updates

--- installs/git_pull_su.sh
+++ installs/git_pull_su.sh
@@ -5,14 +5,22 @@
 # we execute the script which sends the device ssh server and information to the MDM system
 cd ~/vizidef_apks
 
+echo "Pulling latest from repo"
+
 git pull origin master >> ~/bashlog
+
+echo "Setting correct file folder permissions"
 
 chmod -R 771 ~/vizidef_apks
 chmod -R 771 ~/vizidef_apks/*
 
+echo "Performing root operations"
+
 su << EOF 
+echo "removing previous apk"
 rm /data/app/test.txt
 
+echo "transfering newest apk"
 cp ~/vizidef_apks/steelcase/test.txt /data/app/test.txt
 
 echo ""
