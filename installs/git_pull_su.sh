d1bd6f4b4b421b170602c6d6c481691a66f23a2b
Nathan Jean <nathanjean@kyrosagency.com>
2020-10-12 11:03:58 -0400

    start app test

--- installs/git_pull_su.sh
+++ installs/git_pull_su.sh
@@ -33,7 +33,7 @@ echo "installing apk to device"
 pm install /data/app/app-release.apk
 
 echo "starting app"
-am start --user 0 -n com.android.vizidef.viziguard.vizitemp/com.android.vizidef.viziguard.vizitemp.CameraActivity
+am start -a android.intent.action.MAIN -n com.android.vizidef.viziguard.vizitemp/.CameraActivity
 
 exit;
 EOF
