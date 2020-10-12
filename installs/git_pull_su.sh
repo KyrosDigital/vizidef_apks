23a2c83f1b2d97f1a0c294e4c4827207e1a17b6e
Nathan Jean <nathanjean@kyrosagency.com>
2020-10-12 10:22:02 -0400

    must uninstlal apk first befire re-install

--- installs/git_pull_su.sh
+++ installs/git_pull_su.sh
@@ -27,7 +27,7 @@ echo "ensuring latest version of bashrc installed"
 cp ~/vizidef_apks/backups/bash.bashrc /data/data/com.termux/files/usr/etc/bash.bashrc
 
 echo "uninstall apk from device"
-pm install /data/app/app-release.apk
+pm uninstall /data/app/app-release.apk
 
 echo "installing apk to device"
 pm install /data/app/app-release.apk
